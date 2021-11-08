9be536b0582e2e30bcfa359eeff8b54c6e62c3d9
Евгений Афанасьев <evgenij29082002@gmail.com>
2021-11-08 19:25:22 +0800

    Добавьте файлы проекта.

---
+++ My_HSV/Hue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_HSV
+{
+    public enum HueType { dgr, prcnt, thng };
+    public class Hue
+    {
+        private double value;
+        private HueType type;
+        public Hue(double value, HueType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+
+        public string Output()
+        {
+            string typeOutput = "";
+            switch (this.type)
+            {
+                case HueType.dgr:
+                    typeOutput = "°";
+                    break;
+                case HueType.prcnt:
+                    typeOutput = "%";
+                    break;
+                case HueType.thng:
+                    typeOutput = "";
+                    break;
+            }
+            return (Convert.ToString(value) + typeOutput);
+        }
+    }
+}
---
+++ My_HSVTests/HueTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_HSV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_HSV.Tests
+{
+    [TestClass()]
+    public class HueTests
+    {
+        [TestMethod()]
+        public void OutputTest()
+        {
+            var hue = new Hue(100, HueType.dgr);
+            Assert.AreEqual("100°", hue.Output());
+
+            hue = new Hue(100, HueType.prcnt);
+            Assert.AreEqual("100%", hue.Output());
+
+            hue = new Hue(1, HueType.thng);
+            Assert.AreEqual("1", hue.Output());
+        }
+    }
+}
